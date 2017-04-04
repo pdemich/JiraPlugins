6340e9ed58415a5a414a8123ed53460f7317e282
user <user@192.168.1.5>
2017-04-04 21:33:42 +1200

    added two private methods

--- projectx/src/main/java/com/demich/servlet/CustomFieldManager.java
+++ projectx/src/main/java/com/demich/servlet/CustomFieldManager.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 
 import java.net.URI;
 import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.user.UserKey;
 import com.atlassian.sal.api.user.UserManager;
 
 public class CustomFieldManager extends HttpServlet{
@@ -28,7 +29,30 @@ public class CustomFieldManager extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-    	
+    	UserKey userKey = userManager.getRemoteUser(req).getUserKey();
+        if (userKey == null || !userManager.isSystemAdmin(userKey))
+        {
+          redirectToLogin(req, resp);
+          return;
+        }
     }
+
+	private void redirectToLogin(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		resp.sendRedirect(loginUriProvider.getLoginUri(getUri(req)).toASCIIString());
+		
+	}
+
+	private URI getUri(HttpServletRequest req) {
+		
+		StringBuffer builder = req.getRequestURL();
+		  if (req.getQueryString() != null)
+		  {
+		    builder.append("?");
+		    builder.append(req.getQueryString());
+		  }
+		  return URI.create(builder.toString());
+	
+	}
 
 }
