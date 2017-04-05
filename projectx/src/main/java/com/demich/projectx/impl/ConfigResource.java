1a6f05cb5c05c2c4fc6aa81f2bf9c4373419d318
user <user@192.168.1.5>
2017-04-05 15:17:02 +1200

    Added Config resource class implementation

---
+++ projectx/src/main/java/com/demich/projectx/impl/ConfigResource.java
@@ -0,0 +1,47 @@
+package com.demich.projectx.impl;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import javax.inject.Inject;
+
+import com.atlassian.sal.api.pluginsettings.PluginSettings;
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
+import com.atlassian.sal.api.transaction.TransactionCallback;
+import com.atlassian.sal.api.transaction.TransactionTemplate;
+import com.atlassian.sal.api.user.UserManager;
+
+@Path("/")
+@Scanned
+public class ConfigResource
+{
+    @ComponentImport
+    private final UserManager userManager;
+    @ComponentImport
+    private final PluginSettingsFactory pluginSettingsFactory;
+    @ComponentImport
+    private final TransactionTemplate transactionTemplate;
+
+    @Inject
+    public ConfigResource(UserManager userManager, PluginSettingsFactory pluginSettingsFactory,
+                          TransactionTemplate transactionTemplate)
+    {
+        this.userManager = userManager;
+        this.pluginSettingsFactory = pluginSettingsFactory;
+        this.transactionTemplate = transactionTemplate;
+    }
+}
