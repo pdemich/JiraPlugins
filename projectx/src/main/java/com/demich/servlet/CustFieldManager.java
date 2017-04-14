f38644c49038f720122374234c04df15c9ca1bc6
user <user@192.168.1.5>
2017-04-14 22:20:48 +1200

    Added matrix project vs field visibility

--- projectx/src/main/java/com/demich/servlet/CustFieldManager.java
+++ projectx/src/main/java/com/demich/servlet/CustFieldManager.java
@@ -12,7 +12,6 @@ import javax.inject.Inject;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 
-import java.awt.List;
 import java.io.IOException;
 
 import java.net.URI;
@@ -20,6 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
+import java.util.List;
 import java.util.ArrayList;
 
 import com.atlassian.sal.api.auth.LoginUriProvider;
@@ -28,10 +28,13 @@ import com.atlassian.sal.api.user.UserManager;
 import com.atlassian.sal.api.user.UserProfile;
 import com.atlassian.templaterenderer.TemplateRenderer;
 import com.atlassian.jira.util.velocity.VelocityRequestContext;
+import com.atlassian.jira.web.FieldVisibilityManager;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.CustomFieldManager;
 import com.atlassian.jira.issue.fields.NavigableField;
+import com.atlassian.jira.project.Project;
+import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.jira.component.ComponentAccessor;
 
 @Scanned
@@ -43,18 +46,27 @@ public class CustFieldManager extends HttpServlet{
 	private final LoginUriProvider loginUriProvider;
 	@ComponentImport
 	private final TemplateRenderer renderer;
+	@ComponentImport
+	private final FieldVisibilityManager fieldVisibilityManager;
 	
 	private CustomFieldManager cfieldmanager;
+	
+	private ProjectManager projectManager;
+	
 	
 	
 	
 	@Inject
-    public CustFieldManager(UserManager userManager, LoginUriProvider loginUriProvider, TemplateRenderer renderer) {
+    public CustFieldManager(UserManager userManager, LoginUriProvider loginUriProvider, TemplateRenderer renderer, 
+    		FieldVisibilityManager fieldVisibilityManager) {
     	
     	this.userManager = userManager;
     	this.loginUriProvider = loginUriProvider;
     	this.renderer = renderer;
+    	this.fieldVisibilityManager = fieldVisibilityManager;
     	this.cfieldmanager = ComponentAccessor.getCustomFieldManager();
+    	this.projectManager = ComponentAccessor.getProjectManager();
+    	
 	}
     
     @SuppressWarnings("deprecation")
@@ -69,19 +81,32 @@ public class CustFieldManager extends HttpServlet{
           return;
         }
         
+        List<Project> projects = projectManager.getProjects();
+        List<CustomField> fields = cfieldmanager.getCustomFieldObjects();
+        Map<String,Object> hiddenFieldMapping = new HashMap<String,Object>();
         ArrayList<NavigableField> names = new ArrayList<NavigableField>(); 
         
-        java.util.List<CustomField> fields = cfieldmanager.getCustomFieldObjects();
+        for(Project project : projects ) {
+        	ArrayList<Pair<Long,Boolean>> temp = new ArrayList<Pair<Long,Boolean>>();
+        	for(CustomField field : fields) {
+    			temp.add(new Pair<Long, Boolean>(project.getId(), fieldVisibilityManager.isFieldHiddenInAllSchemes(project.getId(), field.getId())));	
+    		}
+        	hiddenFieldMapping.put(project.getName(), temp);
+        }
+        
+        
 		for(CustomField field : fields) {
 			
 			names.add(field);
 		}
-			
+		Boolean wynik = fieldVisibilityManager.isFieldHiddenInAllSchemes(10100L, "customfield_10100");	
 		
      // Create the Velocity Context
         Map<String,Object> context = new HashMap<String,Object>();
         context.put("username", username);
         context.put("fieldnames", names);
+        context.put("projects", projects);
+        context.put("hfmapping", hiddenFieldMapping);
         
         resp.setContentType("text/html;charset=utf-8");
         renderer.render("admin.vm", context, resp.getWriter());
